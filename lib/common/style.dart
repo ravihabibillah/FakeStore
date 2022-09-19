e94b75052d4d0a240158755d68a02ea7215c324e
ravihabibillah <ravihabibillah44@gmail.com>
2022-09-19 13:32:04 +0700

    create style

---
+++ lib/common/style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+Color blackColor = Colors.black;
+Color grayColor = Colors.grey;
+Color yellowColor = Color(0xFFF7C325);
+
+TextStyle blackStyle = TextStyle(color: blackColor);
+TextStyle grayStyle = TextStyle(color: grayColor);
+
+TextStyle boldStyle = blackStyle.copyWith(fontWeight: FontWeight.w600);
