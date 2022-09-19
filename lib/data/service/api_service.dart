3e7b2ea84d1f340e98c38ac9fd7a5a3482d10c1f
ravihabibillah <ravihabibillah44@gmail.com>
2022-09-19 16:47:54 +0700

    create post service

--- lib/data/service/api_service.dart
+++ lib/data/service/api_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:fake_store/data/models/product.dart';
 import 'package:http/http.dart' as http;
 
@@ -5,6 +7,8 @@ class ApiService {
   static var client = http.Client();
   static const String _baseUrl = 'https://fakestoreapi.com/';
   static const String _product = 'products';
+
+  // bool isSuccess = false;
 
   static Future<List<Product>> fetchProducts() async {
     var response = await client.get(Uri.parse(_baseUrl + _product));
@@ -15,6 +19,20 @@ class ApiService {
     } else {
       // show error message
       throw Exception('Failed to show product list');
+    }
+  }
+
+  static Future<bool> addProduct(Product data) async {
+    var response = await client.post(Uri.parse(_baseUrl + _product),
+        headers: {'Content-type': 'application/json'},
+        body: jsonEncode(data.toJson()));
+
+    if (response.statusCode == 200) {
+      // isSuccess = true;
+      return true;
+    } else {
+      // show error message
+      throw Exception('Failed to add product');
     }
   }
 }
