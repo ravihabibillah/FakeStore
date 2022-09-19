c65d26a9e9e9ecbb5a8eb3aa9e91ff4cdf2f5103
ravihabibillah <ravihabibillah44@gmail.com>
2022-09-19 17:51:51 +0700

    change intentWithString to intentWithProduct

--- lib/utils/navigation.dart
+++ lib/utils/navigation.dart
@@ -1,13 +1,15 @@
 
 import 'package:flutter/widgets.dart';
 
+import '../data/models/product.dart';
+
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class Navigation {
   static intentWithData (String routeName, Object arguments) {
     navigatorKey.currentState?.pushNamed(routeName, arguments: arguments);
   }
-  static intentWithString (String routeName, String? arguments) {
+  static intentWithProduct (String routeName, Product arguments) {
     navigatorKey.currentState?.pushNamed(routeName, arguments: arguments);
   }
 
