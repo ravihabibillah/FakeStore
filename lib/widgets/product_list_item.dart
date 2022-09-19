658d301ecdd39eecd3c4ab1e2356297580630eab
ravihabibillah <ravihabibillah44@gmail.com>
2022-09-19 14:30:42 +0700

    create product item

---
+++ lib/widgets/product_list_item.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+import '../common/style.dart';
+import '../data/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16),
+          ),
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
+          height: 190,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    Center(
+                      child: Image.network(
+                        product.image,
+                        width: 100,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(3),
+                      margin: EdgeInsets.only(top: 6, left: 6),
+                      decoration: BoxDecoration(
+                        color: grayColor.withOpacity(0.6),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        '\$${product.price}',
+                        style: boldStyle.copyWith(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10),
+              Flexible(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        product.title,
+                        style: boldStyle.copyWith(fontSize: 20),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        product.description,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 4,
+                        textAlign: TextAlign.justify,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          Spacer(),
+                          Icon(
+                            Icons.star,
+                            size: 16,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            '${product.rating.rate}',
+                            style: boldStyle,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
