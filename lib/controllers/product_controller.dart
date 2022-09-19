e16fa693abe57c4284da64478793ddea15e12648
ravihabibillah <ravihabibillah44@gmail.com>
2022-09-19 16:48:26 +0700

    create add product in controller

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -4,6 +4,11 @@ import '../data/models/product.dart';
 
 class ProductController extends GetxController {
   var isLoading = true.obs;
+  var isError = false.obs;
+  var hasData = false.obs;
+  var noData = true.obs;
+  var message = ''.obs;
+
   var productList = <Product>[].obs;
 
   @override
@@ -12,16 +17,45 @@ class ProductController extends GetxController {
     super.onInit();
   }
 
-  void fetchProducts() async {
+  Future<dynamic> fetchProducts() async {
     isLoading(true);
+    isError(false);
+    hasData(false);
+    noData(true);
     try {
       var products = await ApiService.fetchProducts();
-      if(products.isNotEmpty) {
+      if (products.isNotEmpty) {
         productList.value = products;
+        isLoading(false);
+        hasData(true);
+        noData(false);
+      } else if (noData.value) {
+        message.value = 'There is no data';
       }
-    } finally {
+    } catch (e) {
       isLoading(false);
+      isError(true);
+      return message.value = 'No internet connection';
     }
-
   }
+
+  Future<dynamic> addProduct(Product data) async {
+    isError(false);
+    isLoading(true);
+    try {
+      var isSuccess = await ApiService.addProduct(data);
+      if (isSuccess) {
+        message.value = 'Add product success';
+        isLoading(false);
+      } else {
+        message.value = 'Failed to add product';
+      }
+    } catch (e) {
+      isLoading(false);
+      isError(true);
+      return message.value = 'No internet connection';
+    }
+  }
+
+
 }
